8f90ee80e873954540b2921236bd7c1554acdf69
michael <kimh060612@naver.com>
2022-07-16 16:32:37 +0900

    .

---
+++ src/seeds/chatting.seed.ts
@@ -0,0 +1,17 @@
+import { Connection } from "typeorm";
+import { Factory, Seeder } from 'typeorm-seeding';
+import { ChattingEntity } from "src/common/entity/chatting.entity";
+
+export class CreateInitialChattingData implements Seeder {
+    public async run(factory: Factory, connection: Connection): Promise<void> {
+        await connection
+                .createQueryBuilder()
+                .insert()
+                .into(ChattingEntity)
+                .values([
+                    { contents: 'Hello!', sender: false },
+                    { contents: 'World!', sender: true }
+                ])
+                .execute();
+    }
+}
