8633f7646124ff4002ef3435660399529c448eec
michael <kimh060612@naver.com>
2022-07-16 15:09:24 +0900

    .

--- ormconfig.js
+++ ormconfig.js
@@ -1,3 +1,8 @@
 module.exports = {
+    host: '127.0.0.1',
+    user: 'nearby',
+    password: 'n3@rbYP@sswD12#$',
+    port: 3306,
+    database: 'nearby',
     seeds: ['src/seeds/**/*{.ts,.js}']
 }
