948045a6a7d8cda3bfeb875b1a0158aacd8b1ca7
michael <kimh060612@naver.com>
2022-07-20 00:28:46 +0900

    .

--- src/seeds/seeder.module.ts
+++ src/seeds/seeder.module.ts
@@ -1,12 +1,14 @@
 import { Module, Logger } from '@nestjs/common';
-import { DataSeederModule } from './seedService/seed.module';
 import { Seeder } from './seeder.service';
 import { MySQLDBProviderModule } from '../providers/database/mysql/mysql.module';
+import { CaregiverSeederModule } from './seedService/Caregiver/caregiver.module';
+import { ElderlySeederModule } from './seedService/Elderly/elderly.module';
 
 @Module({
     imports: [
         MySQLDBProviderModule,
-        DataSeederModule
+        CaregiverSeederModule,
+        ElderlySeederModule
     ],
     providers: [ 
         Logger, 
