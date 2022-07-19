ed769db933026b2798782a9a794a383b909e6f05
michael <kimh060612@naver.com>
2022-07-20 00:12:28 +0900

    .

--- src/seeds/seeder.module.ts
+++ src/seeds/seeder.module.ts
@@ -1,12 +1,13 @@
 import { Module, Logger } from '@nestjs/common';
 import { MySQLDBProviderModule } from '../providers/database/mysql/mysql.module';
 import { DataSeederModule } from './seedService/seed.module';
+import { Seeder } from './seeder.service';
 
 @Module({
     imports: [
         MySQLDBProviderModule,
         DataSeederModule
     ],
-    providers: [ Logger ]
+    providers: [ Logger, Seeder ]
 })
 export class SeederModule {}
