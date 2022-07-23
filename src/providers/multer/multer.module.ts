f1fbad05da72ea6e3f22fd95aca1a4379c1cdd22
michael <kimh060612@naver.com>
2022-07-23 22:24:46 +0900

    .

--- src/providers/multer/multer.module.ts
+++ src/providers/multer/multer.module.ts
@@ -1,13 +1,14 @@
 import { Module, Logger } from '@nestjs/common';
 import { MulterModule } from "@nestjs/platform-express";
 import { S3Service } from './s3/s3.service';
-import { AWSProviderModule } from '../aws/aws.module';
+import { AwsSdkModule } from 'nest-aws-sdk';
+import { S3 } from 'aws-sdk';
 
 @Module({
     imports: [
         MulterModule.registerAsync({
             useClass: S3Service,
-            imports: [ AWSProviderModule ]
+            imports: [ AwsSdkModule.forFeatures([S3]) ],
         })
     ]
 })
