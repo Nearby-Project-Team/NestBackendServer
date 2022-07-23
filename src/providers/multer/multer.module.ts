1615a98a5e81439c54ecaae502fe13b67f8007eb
michael <kimh060612@naver.com>
2022-07-23 23:00:38 +0900

    .

--- src/providers/multer/multer.module.ts
+++ src/providers/multer/multer.module.ts
@@ -7,22 +7,22 @@ import { S3 } from 'aws-sdk';
 
 @Module({
     imports: [
-        AwsSdkModule.forRootAsync({
-            defaultServiceOptions: {
-                useFactory: (cs: ConfigService) => {
-                    return {
-                        region: cs.get<string>('AWS_REGION'),
-                        credentials: {
-                            accessKeyId: cs.get<string>('AWS_ACCESS_KEY'),
-                            secretAccessKey: cs.get<string>('AWS_SECRET_KEY')
-                        }
-                    }
-                },
-                imports: [ConfigModule],
-                inject: [ConfigService]
-            },
-            services: [S3]
-        }),
+        // AwsSdkModule.forRootAsync({
+        //     defaultServiceOptions: {
+        //         useFactory: (cs: ConfigService) => {
+        //             return {
+        //                 region: cs.get<string>('AWS_REGION'),
+        //                 credentials: {
+        //                     accessKeyId: cs.get<string>('AWS_ACCESS_KEY'),
+        //                     secretAccessKey: cs.get<string>('AWS_SECRET_KEY')
+        //                 }
+        //             }
+        //         },
+        //         imports: [ConfigModule],
+        //         inject: [ConfigService]
+        //     },
+        //     services: [S3]
+        // }),
         MulterModule.registerAsync({
             useClass: S3Service,
         })
