7234f7fe8ccb48bf7351a4b6e8f49ad881b82470
michael <kimh060612@naver.com>
2022-07-23 15:57:10 +0900

    .

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { GlobalErrorDispatcher } from './common/exceptions/global.exception';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
+  console.log(`Backend ${process.env.NODE_ENV} mode starting...`);
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ 
     transform: true,
