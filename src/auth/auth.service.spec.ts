8f0782276d5344d8bc621456bdc535c6e3630fe6
kyun2024 <cune2024@gmail.com>
2022-08-12 23:38:39 +0900

    Modifed 1

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,4 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { LoginRequestDto } from 'src/common/dtos/caregiver/login-request.dto';
+import { AppError } from 'src/common/error/ErrorEntity/AppError';
+import { AppErrorTypeEnum } from 'src/common/error/ErrorType/AppErrorType.enum';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
@@ -15,4 +18,44 @@ describe('AuthService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('validateCaregiver', () => {
+      it('should be ', () => {
+        const temp_email = "should.be@ema.il";
+        const temp_password = "passwd";
+
+        expect(service.validateCaregiver(temp_email,temp_password)).toBeDefined();
+      });
+  });
+
+  describe('login', () => {
+    it('should be defined',() => {
+      const user = {email : "email", password : "passwd"};
+      expect(service.login(user)).toBeDefined();
+    });
+  });
+
+  describe('register', () => {
+    it('should be defined', () => {
+      const user = {email:"", password:"", name:"", phone_number:""};
+      expect(service.register(user)).toBeDefined();
+    });
+
+    it('should throw user exist error',() => {
+      try{
+        const user = {email:"", password:"", name:"", phone_number:""};
+        service.register(user);
+      }
+      catch(e){
+        expect(e).toBeInstanceOf(AppError);
+        if(e instanceof AppError)
+        {
+          expect(e.errorCode).toEqual(AppErrorTypeEnum.USER_EXISTS);
+        }
+      }
+    });
+  });
+
+  describe('verify', () => {});
+  describe('agreement', () => {});
 });
