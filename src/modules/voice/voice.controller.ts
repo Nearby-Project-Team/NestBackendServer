5b7f09efc7406c18f87f7b661cc2c6abf83e8f06
michael <kimh060612@naver.com>
2022-08-15 16:20:56 +0900

    .

--- src/modules/voice/voice.controller.ts
+++ src/modules/voice/voice.controller.ts
@@ -11,13 +11,13 @@ export class VoiceController {
 
   // 보호자가 음성을 등록함 => 등록 완료되면 O.K 때림.
   @Post('/register/:email/:vname')
-  @UseInterceptors(FileInterceptor('audio', {}))
+  @UseInterceptors(FileInterceptor('audio'))
   async registerVoice(
     @Param('email') email: string,
     @Param('vname') vname: string,
     @UploadedFile() file: Express.Multer.File
   ) {
-    // Save file in 'bucket: nearby-<env>-bucket/caregiver_id/voice_name/voice_file_uuid.wav'
+    // Save file in 'bucket: nearby-<env>-bucket/caregiver_email/voice_name/voice_file_name.wav'
     return this.voiceService.registerVoice(email, vname, file.originalname);
   }
 
