cac3a05ad7299a6ce5c73054b477dc0131fe71d5
michael <kimh060612@naver.com>
2022-08-27 17:23:59 +0900

    Elderly Seed Data

--- src/seeds/seedingData/elderly.seed.ts
+++ src/seeds/seedingData/elderly.seed.ts
@@ -1,13 +1,20 @@
 import { IElderlyEntity } from '../seedingInterface/IElderlyEntity';
 import { AgreementEnum } from 'src/common/types/agreement.type';
+import { faker } from '@faker-js/faker';
 
 export const ElderlySeedData: IElderlyEntity[] = [
     {
         name: 'KimMin',
-        agreement: AgreementEnum.agree
+        agreement: AgreementEnum.agree,
+        phone_number: faker.phone.number("+82 10 #### ####"),
+        birthday: faker.date.birthdate(),
+        token: "123456789"
     },
     {
         name: 'HanWoo',
-        agreement: AgreementEnum.agree
+        agreement: AgreementEnum.agree,
+        phone_number: faker.phone.number("+82 10 #### ####"),
+        birthday: faker.date.birthdate(),
+        token: "123456789"
     }
 ];
--- src/seeds/seedingInterface/IElderlyEntity.ts
+++ src/seeds/seedingInterface/IElderlyEntity.ts
@@ -1,5 +1,9 @@
 import { AgreementEnum } from 'src/common/types/agreement.type';
+
 export interface IElderlyEntity {
     name: string;
     agreement: AgreementEnum;
+    phone_number: string;
+    birthday: Date;
+    token: string;
 }
