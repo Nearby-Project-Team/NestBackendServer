c6c2486ee057661edc474ad279bfd42112737cd4
Michael <kimh060612@autocrypt.io>
2022-09-15 17:44:52 +0900

    ArrayBuffer

--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -96,7 +96,7 @@ export class ChatService {
         // this.logger.debug(`${_url}/tts/inference`);
         this.logger.debug(JSON.stringify(_data));
         const _res = await this.httpService.axiosRef.post(`${_url}/tts/inference`, _data, {
-            responseType: 'blob'
+            responseType: 'arraybuffer'
         });
         return _res;
     }
